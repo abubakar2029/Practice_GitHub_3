1b69dc573e7f756236445c588ccd9f20cad1d318
Abu Bakar <abubakarzafar2029@gmail.com>
2024-01-12 12:37:23 +0500

    Added schema file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server = new ApolloServer({
   // typeDefs
   // resolvers
 });
+// Schema describes the shape of graph and data available on it
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
